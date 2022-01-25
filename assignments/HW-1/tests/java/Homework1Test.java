0aae1f8433ba1669bcdb68dfed94c4d0c037ff4b
Antonio Zea Jr <zeajantonio@gmail.com>
2022-01-25 12:50:57 -0500

    adds parameterized tests
    Instead of creating multiple tests with similar formatting, I will
    create dozens of parameters to be passed into the test.

---
+++ assignments/HW-1/tests/java/Homework1Test.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Homework1Test {
+
+    private static Stream<Arguments> ArraysFindLast() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3}, 2, 1),
+                Arguments.of(new int[]{1, 2, 3}, 1, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ArraysFindLast")
+    void findLast(int[] x, int y, int expected) {
+        assertEquals(expected, Homework1.findLast(x, y));
+    }
+
+    @Test
+    void countOccurrence() {
+    }
+
+    @Test
+    void countPositive() {
+    }
+
+    @Test
+    void addCommasToDigitString() {
+    }
+}
