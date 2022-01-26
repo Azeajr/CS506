4e58cbd23453e0e28b6e7f85903306508679ac95
Antonio Zea Jr <zeajantonio@gmail.com>
2022-01-26 07:59:28 -0500

    add tests for countOccurrence

--- assignments/HW-1/tests/java/Homework1Test.java
+++ assignments/HW-1/tests/java/Homework1Test.java
@@ -11,6 +11,8 @@ class Homework1Test {
 
     private static Stream<Arguments> ArraysFindLast() {
         return Stream.of(
+                Arguments.of(null, 2, -1),
+                Arguments.of(new int[]{}, 2, -1),
                 Arguments.of(new int[]{1, 2, 3}, 2, 1),
                 Arguments.of(new int[]{1, 2, 3}, 1, 0),
                 Arguments.of(new int[]{0, 1, 2, 3}, 1, 1),
@@ -24,8 +26,24 @@ class Homework1Test {
         assertEquals(expected, Homework1.findLast(x, y));
     }
 
-    @Test
-    void countOccurrence() {
+    private static Stream<Arguments> StringsCountOccurence() {
+        return Stream.of(
+                Arguments.of(null, null, 0),
+                Arguments.of("", null, 0),
+                Arguments.of("", "Antonio", 0),
+                Arguments.of("", "", 1),
+                Arguments.of("Antonio Antonio", " ", 0),
+                Arguments.of("Antonio Antonio", "", 0),
+                Arguments.of(" ", " ", 0),
+                Arguments.of("Antonio Antonio", "Antonio", 2),
+                Arguments.of("What is Software Testing? Why is Software Testing so important?", "Software", 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("StringsCountOccurence")
+    void countOccurrence(String source, String word, int expected) {
+        assertEquals(expected, Homework1.countOccurrence(source, word));
     }
 
     @Test
